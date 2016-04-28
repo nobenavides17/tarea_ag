4ea9987929b96a779f6f2fcb263ede2e539b6bbc
LexxRamirez <antoni.the21@gmail.com>
2016-04-27 19:24:48 -0600

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,12 @@ float mat_ambient[] ={ 0.0f,0.1f,0.06f ,1.0f};
 float mat_diffuse[] ={ 0.0f,0.50980392f,0.50980392f,1.0f};
 float mat_specular[] ={0.50196078f,0.50196078f,0.50196078f,1.0f };
 float shine[] ={32.0f};
-          
 
 void init(void)
 {
-	glClearColor(1.0,1.0,1.0,1.0);
-// Ubicamos la fuente de luz en el punto
-//GLfloat luz_ambiental[] = { 0.2, 0.2, 0.2, 1.0 };
+    glClearColor(1.0,1.0,1.0,1.0);
 
-
-// Activamos la fuente de luz
+    // Activamos la fuente de luz
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glDepthFunc(GL_LESS);
@@ -26,40 +22,37 @@ void init(void)
 void reshape(int w, int h)
 {
     glViewport(0, 0,  (GLsizei) w, (GLsizei) h);
-// Activamos la matriz de proyeccion.
+    // Activamos la matriz de proyeccion.
     glMatrixMode(GL_PROJECTION);
-// "limpiamos" esta con la matriz identidad.
+    // "limpiamos" esta con la matriz identidad.
     glLoadIdentity();
-// Usamos proyeccion ortogonal
-	glOrtho (-300, 300, -300*(GLfloat)h/(GLfloat)w,350*(GLfloat)h/(GLfloat)w, -300, 300);
-    
-// Activamos la matriz de modelado/visionado.
+    // Usamos proyeccion ortogonal
+    glOrtho (-300, 300, -300*(GLfloat)h/(GLfloat)w,350*(GLfloat)h/(GLfloat)w, -300, 300);
+    // Activamos la matriz de modelado/visionado.
     glMatrixMode(GL_MODELVIEW);
-// "Limpiamos" la matriz
+    // "Limpiamos" la matriz
     glLoadIdentity();
 
 }
 // Aqui ponemos lo que queremos dibujar.
 void display(void)
 {
-// Propiedades del material
-// "Limpiamos" el frame buffer con el color de "Clear", en este
-// caso negro.
+    // Propiedades del material
+    // "Limpiamos" el frame buffer con el color de "Clear", en este
+    // caso negro.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	    GLfloat light_position[] = { (int)((0.03076923076923076923076923076923 * (n_x + v_x)) - 10), (int)((0.03076923076923076923076923076923 * (n_y + v_y)) - 10) * -1, 1.0, 0.0 };
-
-	glLightfv(GL_LIGHT0,GL_POSITION,light_position);
-
+    GLfloat light_position[] = { (int)((0.03076923076923076923076923076923 * (n_x + v_x)) - 10), (int)((0.03076923076923076923076923076923 * (n_y + v_y)) - 10) * -1, 1.0, 0.0 };
+    glLightfv(GL_LIGHT0,GL_POSITION,light_position);
     glMatrixMode( GL_MODELVIEW_MATRIX );
     glLoadIdentity();
 
-// Rotacion de 20 grados en torno al eje x
+    // Rotacion de 20 grados en torno al eje x
     glRotated(20.0, 1.0, 0.0, 0.0);
-// Rotacion de -30 grados en torno al eje y
+    // Rotacion de -30 grados en torno al eje y
     glRotated(-30.0, 0.0, 1.0, 0.0);
-// Dibujamos una "Tetera" y le aplico el material
+    // Dibujamos una "Tetera" y le aplico el material
     glPushMatrix();
-//setMaterial
+    //setMaterial
     glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
@@ -70,7 +63,7 @@ void display(void)
 
 // Termina la ejecucion del programa cuando se presiona ESC
 void keyboard(unsigned char key, int x, int y)
-{
+{   //Inicio de casos para la seleccion de materiales por teclas
     switch (key)
     {
 	case 'C':
@@ -155,7 +148,8 @@ void mouse(int button, int state, int x, int y)
                 n_y = n_y + v_y;
             }
         }
-    }	
+    }
+//Movimiento del foco o luz
 void mover(int x, int y)
     {
         if(stado == 0)
@@ -168,6 +162,7 @@ void mover(int x, int y)
         v_y = y - ry;
         glutPostRedisplay();
     }
+//Posicion inicial del foco o luz
 void nomover(int x, int y)
     {
         e_x = x;
@@ -179,25 +174,17 @@ void nomover(int x, int y)
 // Main del programa.
 int main(int argc, char **argv)
 {
-// Inicializo OpenGL
+    // Inicializo OpenGL
     glutInit(&argc, argv);
-
-// Activamos buffer simple y colores del tipo RGB
+    // Activamos buffer simple y colores del tipo RGB
     glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH);
-
-// Definimos una ventana de medidas 800 x 600 como ventana
-// de visualizacion en pixels
+    // Definimos el alto y ancho de la ventana
     glutInitWindowSize (800, 600);
-
-// Posicionamos la ventana en la esquina superior izquierda de
-// la pantalla.
+    // Posicion en la que iniciara la ventana en pantalla
     glutInitWindowPosition (0, 0);
-
-// Creamos literalmente la ventana y le adjudicamos el nombre que se
-// observara en su barra de titulo.
+    // Creamos la ventana
     glutCreateWindow ("Tarea Materiales");
-
-// Inicializamos el sistema
+    // Inicializamos el sistema
     init();
 
     glutDisplayFunc(display);
